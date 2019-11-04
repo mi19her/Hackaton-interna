d385882a5d10936547cfebf4ddaf11bd55fe60fa
ayauri29 <albayauri.29@gmail.com>
2019-11-04 18:35:45 -0500

    Firebase

---
+++ my-app/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLzAxiPm0eAIrupIYpu2YVUcakDYya0HI",
+    authDomain: "hackaton-interna.firebaseapp.com",
+    databaseURL: "https://hackaton-interna.firebaseio.com",
+    projectId: "hackaton-interna",
+    storageBucket: "hackaton-interna.appspot.com",
+    messagingSenderId: "99240587176",
+    appId: "1:99240587176:web:91c200d0463613177ff79d",
+    measurementId: "G-HHW5NX863J"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
