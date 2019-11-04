6a5825513ef83ebc1c1558552c26e259ef6b7761
mi19her <milcaph.19@gmail.com>
2019-11-04 18:43:52 -0500

    estructura de carpetas login

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -1,2 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import'../css/stylos.css'
+
+export const Login = () =>{
+    return(
+        <div>
+            <form className="flex">
+             <h1>BELCORP</h1>
+             <p> Bienvenida a la Compañia de Belleza donde puedes ganar mas¡ </p>
+             <input type="text" placeholder= "ingresa tu codigo"/>
+             <input type="text" placeholder="ingresa tu contraseña" />
+             <button>Ingresar</button>             
+            </form>
+        </div>
+    )
+}
