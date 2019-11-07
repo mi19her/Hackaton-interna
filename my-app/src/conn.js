c9799a3d0e5de0cc16085ae4033c28d921ae4f7d
mi19her <milcaph.19@gmail.com>
2019-11-06 19:07:12 -0500

    Updates

--- my-app/src/conn.js
+++
@@ -1,7 +0,0 @@
-const admin = require ('firebase-admin');
-
-const serviceAccount = require ("./hackaton-interna-firebase-adminsdk-2xsjh-f279b0c92f.json");
-admin.initializeApp ({ 
-  credencial: admin.credential.cert (serviceAccount),   
-  databaseURL: "https://hackaton-interna.firebaseio.com",
-});
---
+++ precache-manifest.2d130774517d4080320b66c7c24cf1b5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ff54e66c97540203f3334f430e936a",
+    "url": "/Hackaton-interna/index.html"
+  },
+  {
+    "revision": "67dc390a105eec41d4e0",
+    "url": "/Hackaton-interna/static/css/main.f72a91cd.chunk.css"
+  },
+  {
+    "revision": "cea3a3a65cc58e3a0809",
+    "url": "/Hackaton-interna/static/js/2.05d565eb.chunk.js"
+  },
+  {
+    "revision": "67dc390a105eec41d4e0",
+    "url": "/Hackaton-interna/static/js/main.03c0b7b5.chunk.js"
+  },
+  {
+    "revision": "3be2b085630d66f6e8e6",
+    "url": "/Hackaton-interna/static/js/runtime-main.91a4bbcb.js"
+  },
+  {
+    "revision": "0f3abca92c4dfb284bfeb75b6a0c1426",
+    "url": "/Hackaton-interna/static/media/canastaficha.0f3abca9.png"
+  },
+  {
+    "revision": "f3dfe9af587e548bb6d9ce24e100052c",
+    "url": "/Hackaton-interna/static/media/catalogo.f3dfe9af.jpeg"
+  },
+  {
+    "revision": "7b1252f4f8e140d0f9d25cb734217989",
+    "url": "/Hackaton-interna/static/media/google-plus.7b1252f4.png"
+  },
+  {
+    "revision": "e3b2509b56ae8c218ed47f9828de7836",
+    "url": "/Hackaton-interna/static/media/img1.e3b2509b.jpeg"
+  },
+  {
+    "revision": "e498f22a36f441aff37a2f61c0652f59",
+    "url": "/Hackaton-interna/static/media/img2.e498f22a.jpeg"
+  },
+  {
+    "revision": "7d0ef4c0f2ca3a28da48081e22696b24",
+    "url": "/Hackaton-interna/static/media/message.7d0ef4c0.png"
+  },
+  {
+    "revision": "617fac19c200ec84c18e6ccd0ae13ee7",
+    "url": "/Hackaton-interna/static/media/pavito.617fac19.png"
+  },
+  {
+    "revision": "2db1c16be97d8cf02931ca9e9924afd5",
+    "url": "/Hackaton-interna/static/media/platos.2db1c16b.png"
+  },
+  {
+    "revision": "70333665e5afc35533646eaa8b827b08",
+    "url": "/Hackaton-interna/static/media/wt.70333665.png"
+  }
+]);
